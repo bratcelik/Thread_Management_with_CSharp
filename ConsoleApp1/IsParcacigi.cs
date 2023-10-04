f2fd7ba76889a088fe0a897e64463d48b69027e9
Berat Sercan Çelik <bratcelik@gmail.com>
2023-10-04 14:50:35 +0300

    Add project files.

---
+++ ConsoleApp1/IsParcacigi.cs
@@ -0,0 +1,29 @@
+﻿namespace ConsoleApp1
+{
+    public class IsParcacigi
+    {
+        public int Count;
+        public Thread Thrd;
+
+        public IsParcacigi(string name)
+        {
+            Count = 0;
+            Thrd = new Thread(this.Run);
+            Thrd.Name = name;
+            Thrd.Start();
+        }
+
+        public void Run()
+        {
+            Console.WriteLine(Thrd.Name + " starting");
+
+            do
+            {
+                Thread.Sleep(500);
+                Console.WriteLine("In " + Thrd.Name + ", Count is " + Count);
+                Count++;
+            } while (Count <= 10);
+            Console.WriteLine(Thrd.Name + " terminating.");
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,34 @@
+﻿using ConsoleApp1;
+
+
+
+
+
+/*
+ *  IsAlive     Thread'in çalışma durumunu verir.
+ *  Name        Thread'in adını atar ya da verir.
+ *  Priority    Thread'in çalışma önceliğini atar ya da verir
+ *  Abort       ThreadAbortException hatasını atarak thread'n çalışmasını iptal eder.
+ *  Resume      Beklemeye geçirilmiş thread'in çalışmasının devam etmesini sağlar
+ *  Sleep       Belirtilen süre kadar beklemeye geçer.
+ *  Suspend     Thread'i geçici süre durdurur.
+ */
+
+
+
+Console.WriteLine("Main thread starting.");
+
+IsParcacigi isci = new IsParcacigi("Child 1");
+Thread newThrd = new Thread(isci.Run);
+newThrd.Start();
+
+do
+{
+    Console.Write(".");
+    Thread.Sleep(100);
+
+}while (isci.Count != 10);
+
+Console.WriteLine("Main thread ending.");
+
+Console.ReadLine();
